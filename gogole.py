eaf454222ae1b4d9b68ee1042477303473446a96
elisebuard <elisebuard@users.noreply.github.com>
2017-03-10 17:16:06 +0100

    Update gogole.py

--- gogole.py
+++ gogole.py
@@ -1,39 +1,3 @@
-texte = input()
-
-nbLettres = len(texte)
-
-
-
-i = 0
-for loop in range(nbLettres) :
-    if i <= 1 :
-        newCaractere = chr(ord(texte[loop]) +2)
-        print(newCaractere, end="")
-        i = i + 1
-    else : 
-        break
-
-k = 0
-for loop in range(nbLettres) :
-    if k <= 1 :
-        newCaractere = (ord(texte[loop]) - 9)
-        print(newCaractere, end="")
-        k = k + 1 
-    else :
-      break
-
-
-    texte = texte.upper()
-g = 0
-alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-for loop in range(nbLettres) :
-    if g <= 1 :
-        for eachPos in range(26):
-            if texte[loop] == alphabet[eachPos] :
-                print(alphabet[eachPos + 5], end="")
-                break
-    g = g +1	
-
    
 texte = input()
 
