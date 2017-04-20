0015b6bb12a79c97b2799a4df965f555a3a59c26
superpauline <pauline.voisin@hotmail.fr>
2017-04-21 00:11:18 +0200

    Update gogole.py

--- gogole.py
+++ gogole.py
@@ -1,37 +1,35 @@
 Cryptage :
 
-texte = input()
-nbtexte = len(texte)
+texte = input() # le texte a taper 
+nbtexte = len(texte) # renvoie le nombre de caracteres qui composent le texte (soit le nombre de lettres)
 
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," ",".",",",":","!","?",";","à","é","è","'","\\","\"","1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 n = 0
 cle = "CODE" #modifiable
-nbcle = len(cle)
-quotient = 2// nbcle  #penser a changer le 2 quand on veut faire prendre + de 2 lettres en vigenere = pour le reste
-reste = 2 % nbcle 
+nbcle = len(cle) # decouper la cle en 4 caracteres car il y a 4 lettres dans CODE
+quotient = 2// nbcle  # le quotient de la division du nombres de caracteres afilies a chaque programme de cryptage par le nombre de caracteres de la cle
+reste = 2 % nbcle # le reste de la division du nombre de caracteres afilies a chaque programme de cryptage par le nombre de caracteres de la cle
 
-while n < (nbtexte-1) :
+while n < (nbtexte-1) : # tant que la variable n est inferieure au nombre de caracteres du texte 
     lettres = texte[n:n+2] #de 2 en 2 pour chaque language
-   
-    
-   
-    if n%8 == 0 or n%8 == 1 : # + 2 dans Asci et en minuscule
+       
+
+    if n%8 == 0 or n%8 == 1 : # si le reste de la division de la position du caractere dans le texte par 8 (2 lettre pour 4 programmes) vaut 0 ou 1
         for loop in range(2) : 
-            newCaractere = chr(ord(lettres[loop]) +2)
+            newCaractere = chr(ord(lettres[loop]) +2) # + 2 rang ordre alphabetique dans Asci et en minuscule
             print(newCaractere, end="")  
 		
 			
 
-    elif n%8 == 2 or n%8 == 3: # - 9 dans Asci et en chiffres
+    elif n%8 == 2 or n%8 == 3 :  # et si le reste de la division de la position du caractere dans le texte par 8 vaut 2 ou 3
         for loop in range(2) :
-            newCaractere = (ord(lettres[loop]) - 9)
+            newCaractere = (ord(lettres[loop]) - 9) # - 9 rang dans tableau Asci et en chiffre
             print(newCaractere, end="")
-			
-    
-    elif n%8 == 4 or n%8 == 5: # + 5 dans l'alphabet et en majuscule
+			    
+    elif n%8 == 4 or n%8 == 5: 
         for loop in range(2):
             for eachPos in range(49):                 if lettres[loop].upper() == alphabet[eachPos] :
-                    print(alphabet[eachPos + 5], end="")
+                    print(alphabet[eachPos + 5], end="") # + 5 dans notre aplbate qui comprend lettres et caracteres speciaux et en majuscule
 
 		
     elif n%8 == 6 or n%8 == 7 :  # vigenere
@@ -50,7 +48,6 @@ while n < (nbtexte-1) :
                  for eachPos in range(49): 
                     if lettres[loopi+nbcle*quotient].upper() == alphabet[eachPos] :
                         print(alphabet[eachPos + nvlcle],end="")
-    
     n = n + 2
 
 if nbtexte %2 == 1 : #nbr de caractere du mot impaire
