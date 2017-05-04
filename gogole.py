ef52ccb4d60ca5490259af688117ef0a6c32b647
superpauline <pauline.voisin@hotmail.fr>
2017-05-04 19:12:53 +0200

    Update gogole.py

--- gogole.py
+++ gogole.py
@@ -1,7 +1,7 @@
-Cryptage :
+#Cryptage :
 	
-texte = input()
-nbtexte = len(texte)
+texte = input() #utilisateur doit rentrer le texte qu'il souhaite coder
+nbtexte = len(texte) # fonction qui permet de retourner la longueur de la chaine de caracteres
 
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
 
@@ -9,45 +9,52 @@ alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
 
             "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 
-            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] # notre alphabet specifique 
 
-n = 0
-cle = "CODE"  # modifiable
-nbcle = len(cle)
-quotient = 2 // nbcle  # penser a changer le 2 quand on veut faire prendre + de 2 lettres en vigenere = pour le reste
-reste = 2 % nbcle
-
-
-
-def asci_lettres() :
-
-    for loop in range(2) :
-
-            newCaractere = chr(ord(lettres[loop]) + 2)
-
-            print(newCaractere, end="")
+n = 0 # variable ou curseur qui selon son reste par la division euclidienne permet de definir le caractere du texte a coder 
+cle = "CODE"  # utile pour le programme de Vigenere
+nbcle = len(cle) # retourner la longueur de la chaine de caracteres
+quotient = 2 // nbcle  # on divise le nombre le nombre de lettres codees dans un meme programme de codage par le nombre de caracteres de la cle  
+reste = 2 % nbcle # reste de la division de 2 par le nombre de caracteres de la cle 4
 
 
 
-def asci_diminu() :
+def asci_lettres() : #fonction qui transforme deux caracteres cote a cote en deux lettres contenues dans le tableau ASCII
 
-    for loop in range(2):
+    for loop in range(2) : #boucle qui tourne deux fois pour coder les deux caracteres
 
-        newCaractere = chr(ord(lettres[loop]) - 9)
+            newCaractere = chr(ord(lettres[loop]) + 2) 
+		# ord = fonction qui transforme la lettre associee au caractere a son chiffre associe dans ASCII
+		# chr = fonction qui transforme chaque caractere en associant le chiffre du tableau ASCII en sa lettre associee
+		# +2 = avancer de 2 rangs pour avoir une nouvelle lettre
 
-        print(newCaractere, end="")
+
+            print(newCaractere, end="") # sortir les 2 nouveaux caracteres codes 
 
 
 
-def notre_alphabet() :
+def asci_diminu() : # meme fonction mais cette fois sous forme de soustraction
 
     for loop in range(2):
 
-        for eachPos in range(49):
+        newCaractere = chr(ord(lettres[loop]) - 9) # meme chose que precedemment en utilisant le tableau ASCII
+						   # mais cette fois on soustrait de 9 rangs pour avoir une nouvelle lettre
 
-            if lettres[loop].upper() == alphabet[eachPos]:
+        print(newCaractere, end="") # sortir les 2 nouveaux caracteres codes 
 
-                print(alphabet[eachPos + 5], end="")
+
+
+def notre_alphabet() : # fontion qui se refere au tableau que nous avons cree, qui compte les caracteres speciaux
+
+    for loop in range(2):
+
+        for eachPos in range(50): # boucle qui tourne entierement dans notre tableau unique de 50 caracteres
+
+            if lettres[loop].upper() == alphabet[eachPos]: # condition
+							   # si le caractere dans le texte sous forme majuscule 
+							   # est identique a un des 50 caracteres dans notre tableau
+
+                print(alphabet[eachPos + 5], end="") # afficher 2 nouveaux caracteres de 5 rang superieur
 
 
 
@@ -87,11 +94,13 @@ def vige_nere() :
 
 
 
-def texte_impaire() :
+def texte_impaire() : # puisque chaque programme code pour 2 caracteres cote a cote, si le texte est impaire alors 
+		      # la derniere lettre va etre codee selon une fonction specifique
 
-    dernierelettre = chr(ord(texte[nbtexte - 1]) + 6)
+    dernierelettre = chr(ord(texte[nbtexte - 1]) + 6) # selon le tableau ASCII, convertir en chiffre puis en lettre 
+						      # puis avancer de 6 rang
 
-    print(dernierelettre, end="")
+    print(dernierelettre, end="") # afficher le dernier caractere code 
 
 
 
@@ -134,7 +143,7 @@ if nbtexte % 2 == 1:
     texte_impaire()
 
 	
-Décryptage :
+#Décryptage :
 	
 
 texte = input()
